7e29bd6e885c161ff314f9af26d38ddcd03fffe6
Paper-HY <tirpitzz@icloud.com>
2023-07-08 16:56:38 +0100

    asdfxcf

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
-use axum::{routing::get, Router};
-use tower_http::services::ServeDir;
+use axum::{routing::get, routing::get_service, Router};
+use tower_http::services::{ServeDir, ServeFile};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -9,13 +9,19 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn axum(
-    // Name your static assets folder by passing `folder = <name>` to `StaticFolder`
-    // If you don't pass a name, it will default to `static`.
     #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: PathBuf,
 ) -> shuttle_axum::ShuttleAxum {
+    let serve_dir = ServeDir::new(static_folder.clone());
+    let fall = static_folder
+        .clone()
+        .into_os_string()
+        .into_string()
+        .unwrap()
+        + "/index.html";
     let router = Router::new()
-        .route("/", get(hello_world))
-        .nest_service("/assets", ServeDir::new(static_folder));
+        .route("/", get(|| async { "test" }))
+        .nest_service("/assets", serve_dir)
+        .fallback_service(ServeFile::new(fall));
 
     Ok(router.into())
 }
